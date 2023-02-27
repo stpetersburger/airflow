0c902f089a19cbf8ea6d2b876f539aaa754b77c9
ilya <ichazky@gmail.com>
2023-02-27 10:46:26 +0400

    adding category_l1 mapping to the dim_product_categories

--- pyprojects/datawarehouse/etl_queries/dim_product_categories.py
+++ pyprojects/datawarehouse/etl_queries/dim_product_categories.py
@@ -6,7 +6,10 @@ SELECT  id_category      id_product_category,
  GROUP  BY 1,2
 )
 
-SELECT  id_product_category,
-        STRING_AGG(category_name_en, "|") category_name_en
-  FROM  cats
+SELECT  a.id_product_category  id_product_category,
+        STRING_AGG(b.category_l1_name_en, "|") category_L1_name_en,
+        STRING_AGG(a.category_name_en, "|") category_name_en
+  FROM  cats a
+  LEFT  JOIN gcp_gs.map_product_categories b
+        ON a.id_product_category = CAST(b.id_product_category as INT64)
  GROUP  BY 1
