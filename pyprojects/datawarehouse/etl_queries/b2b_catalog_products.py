8b98354830566c012231afbbeaf125b2740ed7b5
ilya <ichazky@gmail.com>
2023-05-12 18:06:26 +0400

    adding b2b catalog_product

--- pyprojects/datawarehouse/etl_queries/b2b_catalog_products.py
+++ pyprojects/datawarehouse/etl_queries/b2b_catalog_products.py
@@ -1,29 +1,22 @@
 WITH products AS (
-SELECT  MAX(pa.abstract_sku)                            id_sku_config,
-        pc.concrete_sku                                 id_sku_simple,
-        MAX(pa.category_key)                            id_category,
-        MAX(c.name_en__us)                              category_name_en,
-        MAX(pa.value_1_en__us)                          brand_name_en,
-        MAX(pa.name_en__us)                             config_name_en,
-        MAX(pc.name_en__us)                             simple_name_en,
-        SUM(COALESCE(s.quantity,0))                     simple_quantity,
-        CASE WHEN MAX(pbaas.approval_status) = 'approved'
-             THEN 1 ELSE 0 END                          if_simple_active,
-        STRING_AGG(CAST(p.value_net as STRING), "@")    net_price,
-        STRING_AGG(CAST(p.value_gross as STRING), "@")  gross_price,
-        MAX(pa.value_1_en__us)                          merchant_name_en
-  FROM  aws_s3.b2b_product_abstract pa
-  LEFT  JOIN aws_s3.b2b_product_concrete pc
-        USING (abstract_sku)
-  LEFT  JOIN aws_s3.b2b_product_abstract_approval_status pbaas
-        ON pbaas.sku = pa.abstract_sku
-  LEFT  JOIN aws_s3.b2b_product_stock s
-        USING (concrete_sku)
-  LEFT  JOIN aws_s3.b2b_product_price p
-        USING (concrete_sku)
-  LEFT  JOIN aws_s3.b2b_category c
-        USING (category_key)
- GROUP  BY 2)
+SELECT  SPLIT(pdf.concrete_sku,'-')[OFFSET(0)]                  id_sku_config,
+        pdf.concrete_sku                                        id_sku_simple,
+        STRING_AGG(LOWER(pdf.category_name), '|')               id_category,
+        STRING_AGG(pdf.category_name, '|')                      category_name_en,
+        STRING_AGG(padf.attribute_value, '|')                   brand_name_en,
+        STRING_AGG(concrete_product_name, '|')                  config_name_en,
+        STRING_AGG(concrete_product_name, '|')                  simple_name_en,
+        0                                                       simple_quantity,
+        MAX(concrete_product_active)                            if_simple_active,
+        STRING_AGG(CAST(pdf.offer_price_net as STRING), "@")    net_price,
+        STRING_AGG(CAST(pdf.offer_price_gross as STRING), "@")  gross_price,
+        STRING_AGG(CAST(pdf.merchant_ref as STRING), '|')       merchant_name_en,
+  FROM  aws_s3.b2b_product_data_feed_EN pdf
+  LEFT  JOIN aws_s3.b2b_product_attribute_data_feed_EN padf
+        ON SPLIT(pdf.concrete_sku,'-')[OFFSET(0)] = padf.sku
+        AND padf.attribute_name='brand'
+ GROUP  BY 2
+)
 
 SELECT  id_sku_config,
         id_sku_simple,
@@ -37,7 +30,7 @@ SELECT  id_sku_config,
         COALESCE(CAST(SPLIT(gross_price,'@')[OFFSET(0)] AS FLOAT64), 0) simple_price,
         COALESCE(CAST(SPLIT(gross_price,'@')[OFFSET(0)] AS FLOAT64), 0) gross_default_price,
         COALESCE(CAST(SPLIT(gross_price,'@')[OFFSET(1)] AS FLOAT64), 0) gross_original_price,
-        COALESCE(CAST(SPLIT(net_price,'@')[OFFSET(0)] AS FLOAT64), 0) net_default_price,
-        COALESCE(CAST(SPLIT(net_price,'@')[OFFSET(1)] AS FLOAT64), 0) net_original_price,
-        merchant_name_en
+        COALESCE(CAST(SPLIT(net_price,'@')[OFFSET(0)] AS FLOAT64), 0)   net_default_price,
+        COALESCE(CAST(SPLIT(net_price,'@')[OFFSET(1)] AS FLOAT64), 0)   net_original_price,
+        COALESCE(merchant_name_en, brand_name_en)                       merchant_name_en
   FROM  products
