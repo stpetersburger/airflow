3e272b2c496b08b74cff46647ea79f8d39f9d7e3
ilya <ichazky@gmail.com>
2023-09-07 15:43:00 +0400

    adding changes for compose file and omniyat_analytics

---
+++ pyprojects/datawarehouse/etl_queries/fact_funnel_ga_monthly_page_traffic.py
@@ -0,0 +1,21 @@
+WITH event_stg AS (
+    SELECT  DATE(DATE_ADD(event_timestamp, INTERVAl 3 HOUR)) event_date_nk,
+            ga_session_id,
+            user_pseudo_id,
+            LEFT(page_location, 60)                          reduced_page_location
+      FROM  gcp_ga.{event_name}
+     WHERE  DATE(DATE_ADD(event_timestamp, INTERVAl 3 HOUR)) >= DATE_SUB(DATE(DATE_TRUNC(DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 3 HOUR), MONTH)), INTERVAL {incr_interval})
+)
+
+SELECT  DATE_TRUNC(estg.event_date_nk, MONTH)             event_date_nk,
+        '{event_name}'                                    event_name,
+        estg.reduced_page_location                        page_location
+        COALESCE(LOWER(molg.page_location), 'untracked')  page_location_tracked,
+        COUNT(estg.user_pseudo_id)                        number_of_users,
+        COUNT(DISTINCT estg.user_pseudo_id)               number_of_unique_users,
+        COUNT(estg.ga_session_id)                         number_of_sessions,
+        COUNT(DISTINCT estg.ga_session_id)                number_of_unique_sessions
+  FROM  event_stg estg
+  LEFT  JOIN gcp_gs.map_{business_type}_location_groups molg
+        ON LOWER(estg.reduced_page_location) LIKE CONCAT(LOWER(molg.page_location),'%')
+ GROUP  BY 1,3,4,5,6,7,8,9,10
