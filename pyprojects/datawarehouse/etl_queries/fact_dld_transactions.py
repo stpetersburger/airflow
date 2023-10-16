9e4c9150e26c765f6f768ca951ec893c990c17ad
stpetersburger <ichazky@gmail.com>
2023-10-16 16:50:02 +0400

    adding transactions fact

---
+++ pyprojects/datawarehouse/etl_queries/fact_dld_transactions.py
@@ -0,0 +1,13 @@
+SELECT  DATE(DATE_ADD(CAST(instance_date as TIMESTAMP), INTERVAL 4 HOUR))  transaction_date_nk,
+        group_en                                                           transaction_type_name_en,
+        procedure_en                                                       transaction_subtype_name_en,
+        usage_en                                                           deal_type_name_en,
+        area_en                                                            district_name_en,
+        prop_type_en                                                       property_type_name_en,
+        prop_sb_type_en                                                    property_sybtype_name_en,
+        rooms_en                                                           property_room_number_en,
+        is_offplan_en                                                      property_completion_name_en,
+        trans_value                                                        transaction_value_aed
+  FROM  scrapers.dld_transactions
+ WHERE  DATE(DATE_ADD(CAST(instance_date as TIMESTAMP), INTERVAL 4 HOUR) >=
+        DATE_ADD(DATE(DATE_ADD(CAST(instance_date as TIMESTAMP), INTERVAL 4 HOUR)), INTERVAL {incr_interval})
