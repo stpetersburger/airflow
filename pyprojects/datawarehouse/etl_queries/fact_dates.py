60761b91f240f0ac061ab772c649c1dc92d1d0b0
stpetersburger <ichazky@gmail.com>
2023-10-17 17:19:15 +0400

    fixing fact_dates

--- pyprojects/datawarehouse/etl_queries/fact_dates.py
+++ pyprojects/datawarehouse/etl_queries/fact_dates.py
@@ -7,16 +7,17 @@ WITH dates AS (
                  )                                     generated_dates
 )
 
-SELECT  generated_dates                         date_nk,
-        FORMAT_DATE('%x',generated_dates)       date_nk_usa,
-        FORMAT_DATE('%Y%m%d', generated_dates)  date_code,
-        FORMAT_DATE('%Y', generated_dates)      date_year_num,
-        FORMAT_DATE('%m', generated_dates)      date_month_num,
-        FORMAT_DATE('%B', generated_dates)      date_month_name,
-        FORMAT_DATE('%b', generated_dates)      date_month_name_short,
-        FORMAT_DATE('%Y-%m', generated_dates)   year_month_nk,
-        FORMAT_DATE('%Y%m', generated_dates)    year_month_code,
-        FORMAT_DATE('%Y-%b', generated_dates)   year_month_name_short,
-        FORMAT_DATE('%W', generated_dates)      date_week_num,
-        FORMAT_DATE('%V', generated_dates)      date_week_num_iso
+SELECT  generated_dates                                       date_nk,
+        FORMAT_DATE('%x',generated_dates)                     date_nk_usa,
+        FORMAT_DATE('%Y%m%d', generated_dates)                date_code,
+        FORMAT_DATE('%Y', generated_dates)                    date_year_num,
+        FORMAT_DATE('%m', generated_dates)                    date_month_num,
+        FORMAT_DATE('%B', generated_dates)                    date_month_name,
+        FORMAT_DATE('%b', generated_dates)                    date_month_name_short,
+        FORMAT_DATE('%Y-%m', generated_dates)                 year_month_nk,
+        FORMAT_DATE('%Y%m', generated_dates)                  year_month_code,
+        FORMAT_DATE('%Y-%b', generated_dates)                 year_month_name_short,
+        FORMAT_DATE('%W', generated_dates)                    date_week_num,
+        FORMAT_DATE('%V', generated_dates)                    date_week_num_iso,
+        CAST(FORMAT_DATE('%Y-%W', generated_dates)AS STRING)  year_week_nk
   FROM  dates
