7a2fccbe9de6b054690a1e8e9105a4f915c0235e
stpetersburger <ichazky@gmail.com>
2024-02-29 16:48:31 +0400

    fixing catalog new csv format

--- pyprojects/datawarehouse/etl_queries/dim_products.py
+++ pyprojects/datawarehouse/etl_queries/dim_products.py
@@ -1,19 +1,30 @@
-SELECT  MAX(id_category)                                fk_product_category,
-        COALESCE(id_sku_simple, 'undefined')            sku,
-        MAX(merchant_name_en)                           merchant_name_en,
-        MAX(brand_name_en)                              brand,
-        MAX(simple_name_en)                             simple_name_en,
-        MAX(if_simple_active)                           if_sku_active
+WITH catalog_stg AS (
+SELECT  id_category                           fk_product_category,
+        COALESCE(id_sku_simple, 'undefined')  sku,
+        merchant_name_en,
+        brand_name_en,
+        simple_name_en,
+        if_simple_active
   FROM  aws_s3.{0}_catalog_products
  GROUP  BY 2
 UNION ALL
-SELECT  MAX(id_category)                                fk_product_category,
-        COALESCE(id_sku_simple, 'undefined')            sku,
-        MAX(merchant_name_en)                           merchant_name_en,
-        MAX(brand_name_en)                              brand,
-        MAX(simple_name_en)                             simple_name_en,
-        MAX(CAST(CASE WHEN LENGTH(if_simple_active) >1
-                      THEN '0' ELSE  if_simple_active
-                 END AS INT64))                         if_sku_active
+SELECT  id_category                                 fk_product_category,
+        COALESCE(id_sku_simple, 'undefined')        sku,
+        merchant_name_en,
+        brand_name_en,
+        simple_name_en,
+        CAST(CASE WHEN LENGTH(if_simple_active) >1
+                  THEN '0' ELSE  if_simple_active
+              END AS INT64)                         if_simple_active
   FROM  aws_s3.{0}_catalog_products_vendure
+ GROUP  BY 2
+)
+
+SELECT  MAX(fk_product_category)  fk_product_category,
+        sku,
+        MAX(merchant_name_en)     merchant_name_en,
+        MAX(brand_name_en)        brand,
+        MAX(simple_name_en)       simple_name_en,
+        MAX(if_simple_active)     if_sku_active
+  FROM  catalog_stg
  GROUP  BY 2
